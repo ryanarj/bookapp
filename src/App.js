09a138021fc85cc791ebd929298295a9b593040d
ryanarjun <karshan813@gmail.com>
2018-05-24 15:15:52 -0400

    added home and login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import {Route} from 'react-router';
+import Home from './components/pages/Home';
+import Login from './components/pages/Login';
 
-const App = () => <div>Hello</div>;
+const App = () => <div>
+   <Route path="/" exact component={Home} />
+   <Route path="/login" exact component={Login} />
+  </div>;
 
 export default App;
