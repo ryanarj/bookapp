177e6ea7b94f224f90302563c8120ff9bc359fd1
ryanarjun <karshan813@gmail.com>
2018-05-24 15:27:32 -0400

    Sementic setup for ui

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import {Route} from 'react-router';
 import Home from './components/pages/Home';
 import Login from './components/pages/Login';
 
-const App = () => <div>
-   <Route path="/" exact component={Home} />
-   <Route path="/login" exact component={Login} />
+const App = () =>   
+  <div className="ui container">
+    <Route path="/" exact component={Home} />
+    <Route path="/login" exact component={Login} />
   </div>;
 
 export default App;
