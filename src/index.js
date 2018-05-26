f224b58c0b050dcfa07c1128887d9cfa0fdf7098
ryanarjun <karshan813@gmail.com>
2018-05-26 18:25:33 -0400

    Added redux devtools

--- src/index.js
+++ src/index.js
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension'
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <Provider store={store}><App /></Provider>
     </BrowserRouter>, 
     document.getElementById('root'));
 registerServiceWorker();
